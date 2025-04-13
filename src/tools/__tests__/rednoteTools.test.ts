a19eb7bdc78b02d31973c37df22da3de9429a2cd
boyan <wby@ojbk.io>
2025-04-13 18:20:15 +0800

    add style lint & rednote share url extract func

--- src/tools/__tests__/rednoteTools.test.ts
+++ src/tools/__tests__/rednoteTools.test.ts
@@ -1,19 +1,20 @@
-import { RedNoteTools, Note, Comment } from "../rednoteTools";
+import { RedNoteTools, Note, Comment } from '../rednoteTools'
 
 // npm run test
-describe("RedNoteTools", () => {
-  let redNoteTools = new RedNoteTools();
+describe('RedNoteTools', () => {
+  let redNoteTools = new RedNoteTools()
 
   afterAll(async () => {
-    await redNoteTools.cleanup();
-  });
+    await redNoteTools.cleanup()
+  })
 
-  test("getNoteContent 应该返回笔记详情", async () => {
-    const url =
-      "https://www.xiaohongshu.com/explore/67da6467000000000602ae8a?xsec_token=ABf-FtTNcXVsJSMq46LttqNovwthdUp5U3z3OklInvWTY=&xsec_source=pc_feed"; // 需要替换为实际笔记URL
-    const note = await redNoteTools.getNoteContent(url);
-    console.log(note);
-    expect(note).toBeDefined();
-    expect(note.url).toBe(url);
-  }, 600000);
-});
+  test('getNoteContent 应该返回笔记详情', async () => {
+    const urlContent = '' // 需要替换为实际笔记URL
+
+    const url = redNoteTools.extractRedBookUrl(urlContent)
+    const note = await redNoteTools.getNoteContent(url)
+    console.log(note)
+    expect(note).toBeDefined()
+    expect(note.url).toBe(url)
+  }, 600000)
+})
